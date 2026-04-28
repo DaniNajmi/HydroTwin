23a0e7818c11f410a3677a680b69e2c12374ff04
Daniela Najmias Lang <dnajmiasl@gmail.com>
2026-04-28 01:31:32 -0500

    Fix caption

--- app/app.py
+++ app/app.py
@@ -29,10 +29,10 @@ df = load_data()
 
 st.title("HydroTwin Monitoring System")
 st.caption(
-    "Interactive aquaponics monitoring system for prediction, anomaly detection, "
-    "state classification, and trust scoring.  \n"
     "Developed by Daniela Najmias Lang"
 )
+st.subheader(    "Interactive aquaponics monitoring system for prediction, anomaly detection, "
+    "state classification, and trust scoring.  \n")
 st.subheader("Ammonia values are shown in PPM (parts per million)")
 
 with st.expander("How to use this dashboard", expanded=True):
